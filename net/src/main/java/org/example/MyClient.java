cb57a6ba9e61db7b66ddb7931c286319a6ee2c0f
jiejiecool <772970200@qq.com>
2025-08-19 22:48:19 +0800

    update

--- net/src/main/java/org/example/MyClient.java
+++ net/src/main/java/org/example/MyClient.java
@@ -1,4 +1,44 @@
 package org.example;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
+import java.net.Socket;
+
+@Slf4j
 public class MyClient {
+
+    private String host;
+    private int port;
+
+    private Socket socket;
+
+    public MyClient setRemoteServer(String remoteIp, int remotePort) {
+       this.host = remoteIp;
+       this.port = remotePort;
+       return this;
+    }
+
+    public static void main(String[] args) throws IOException {
+        MyClient client = new MyClient().setRemoteServer("localhost", 8888);
+        client.connect();
+        client.sendMsg("你好啊");
+
+    }
+    public void connect() throws IOException {
+        this.socket = new Socket(host, port);
+    }
+
+    public void sendMsg(String msg) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        bufferedWriter.write(msg+"\n");
+        bufferedWriter.flush();
+        log.info("发送数据到服务端:{}",msg);
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String result = bufferedReader.readLine();
+        log.info("接收到服务端的响应:{}",result);
+    }
+
+
 }
--- net/src/main/java/org/example/MyServer.java
+++ net/src/main/java/org/example/MyServer.java
@@ -3,8 +3,7 @@ package org.example;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -17,15 +16,26 @@ public class MyServer {
         this.port = port;
     }
 
+    public static void main(String[] args) {
+        new MyServer(8888).start();
+    }
+
     public void start() {
-        try (ServerSocket serverSocket = new ServerSocket(port)){
+        try {
+            log.info("开启服务器....");
+            ServerSocket serverSocket = new ServerSocket(port);
             Socket client = serverSocket.accept();
             InputStream inputStream = client.getInputStream();
-            byte[] arr = new byte[1024];
-            int len = -1;
-            while ((len = inputStream.read(arr)) != -1) {
-
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String s;
+            while ((s = bufferedReader.readLine()) != null) {
+                log.info("读取到数据:{}", s);
+                OutputStream outputStream = client.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+                bufferedWriter.write("我已经收到你的消息了\n");
+                bufferedWriter.flush();
             }
+
 
 
         } catch (IOException e) {
